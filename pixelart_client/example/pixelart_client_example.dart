bb59e23cf58cd2bb38082ccadebdf4fa88899dbd
Emil Noren <stratosfloor@hotmail.com>
2023-10-05 16:02:16 +0200

    Add some exampled of how to use repository

--- pixelart_client/example/pixelart_client_example.dart
+++ pixelart_client/example/pixelart_client_example.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 import 'package:pixelart_client/pixelart_client.dart';
 import 'package:pixelart_shared/pixelart_shared.dart';
 import 'package:uuid/uuid.dart';
@@ -9,8 +10,8 @@ void main() async {
 
   // Create a new PixelArt object
   PixelArt art = PixelArt(
-    id: uuid.v4(),
-    name: uuid.v4(),
+    id: 'emil',
+    name: 'emil',
     description: uuid.v4(),
     width: 64,
     height: 64,
@@ -38,11 +39,74 @@ void main() async {
         "Unable to connect to server. Is the server up and running? Start with 'dart_frog dev'");
     print("Exiting. Try again.");
     exit(255);
+  } else {
+    print('Connected to server.');
   }
 
   // TODO: 20. Use the create/read/list/update/delete/changes methods of the repository to show how it is supposed to be used.
+
+  // Create pixelArt
+  final newPixelArt = await repository.create(art);
+  print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  print('CREATE NEW PIXELART:');
+  print('');
+  print(newPixelArt.value);
+  print('');
+
+  // Read pixelArt
+  final pixelArtFromRepo = await repository.read('emil');
+  print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  print('READ PIXELART:');
+  print('');
+  print(pixelArtFromRepo.value);
+  print('');
+
+  // Update pixelart
+  final updated = pixelArtFromRepo.value?.copyWith(name: noren.name);
+  repository.update(art.id, updated!);
+  print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  print('UPDATE PIXELART:');
+  print('');
+  print(updated);
+  print('');
+
+  // list pixelart
+  final list = await repository.list();
+  print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  print('LIST PIXELART:');
+  print('');
+  print(list.value?.length);
+  // print(list.value);
+  print('');
+
+  // Delete pixelArt
+  final deleted = await repository.delete(art.id);
+  print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  print('DELETE PIXELART:');
+  print('');
+  print('Delete was a ${deleted.status}');
+  print('');
 
   // Exit the application
   print('Exiting application.');
   exit(0);
 }
+
+int getRandomInt() {
+  return Random().nextInt(255);
+}
+
+// Randomly generated pixel
+Pixel randomPixel(Participant participant) {
+  return Pixel(
+    red: getRandomInt(),
+    green: getRandomInt(),
+    blue: getRandomInt(),
+    alpha: getRandomInt(),
+    placedBy: getRandomInt() % 2 == 0 ? emil : noren,
+  );
+}
+
+// Set up participants
+final emil = Participant(id: '01', name: 'Emil');
+final noren = Participant(id: '02', name: 'Noren');
